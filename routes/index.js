24dad922598321f927d84af55890cd08d3be0436
TheInfamousGrim <finchergeorge1@gmail.com>
2022-11-22 18:46:38 +0000

    Create edit store form and add routes
    Created the edit store form template in pug.
    
    Added the API endpoints in the routes.
    
    Started to create the logic to process the store data when a store is
    added or edited.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 
-// Do work here
+/* ------------------------------- Home route ------------------------------- */
 router.get('/', storeController.homePage);
+
+/* ---------------------------- Add store routes ---------------------------- */
+// Main route for add store page
+router.get('/add', storeController.addStore);
+// POST route for the add store data
+router.post('/add', storeController.addStore);
 
 module.exports = router;
