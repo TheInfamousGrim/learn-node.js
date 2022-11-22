158f12808990f4dd2100516dba4ade4dd866654b
TheInfamousGrim <finchergeorge1@gmail.com>
2022-11-22 19:22:11 +0000

    Set store form to save new store to DB
    Created an async function that will save the new store data to the db.
    Wrapped it a higher order function so that any errors will be caught.
    
    Found a bug in the routes. Was using the addStore get route instead
    of the createStore post route that I created.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,12 +1,17 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
-    res.render('index');
+    res.render('index', { title: 'Home' });
 }
 
 exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store' });
 }
 
-exports.createStore = (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
+exports.createStore = async (req, res) => {
+    const store = new Store(req.body);
+    console.log(store);
+    await store.save();
+    res.redirect('/');
 }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 /* ------------------------------- Home route ------------------------------- */
 router.get('/', storeController.homePage);
@@ -9,6 +10,6 @@ router.get('/', storeController.homePage);
 // Main route for add store page
 router.get('/add', storeController.addStore);
 // POST route for the add store data
-router.post('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
