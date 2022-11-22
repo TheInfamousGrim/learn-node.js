baf600ed11368d8c804aa4a412af9371687b8928
TheInfamousGrim <finchergeorge1@gmail.com>
2022-11-22 21:43:38 +0000

    Add getStores controller, create store templates
    Created the controllers for the store page. Queried the db to find
    all the stores.
    
    Created the templates for the stores as well. Created a mixin for the
    store card templates.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,16 +1,26 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
+/* ---------------------------- home controllers ---------------------------- */
 exports.homePage = (req, res) => {
     res.render('index', { title: 'Home' });
 }
 
+/* ----------------------- add store page controllers ----------------------- */
 exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store' });
 }
 
+// Create store post route
 exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save();
     req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`)
     res.redirect(`/store/${store.slug}`);
+}
+
+/* ------------------------- store page controllers ------------------------- */
+exports.getStores = async (req, res) => {
+    // 1. Query the db for a list of all the stores
+    const stores = await Store.find();
+    res.render('stores', { title: 'Stores', stores })
 }
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,15 @@ const storeController = require('../controllers/storeController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 /* ------------------------------- Home route ------------------------------- */
-router.get('/', storeController.homePage);
+router.get('/', catchErrors(storeController.getStores));
 
 /* ---------------------------- Add store routes ---------------------------- */
 // Main route for add store page
 router.get('/add', storeController.addStore);
 // POST route for the add store data
 router.post('/add', catchErrors(storeController.createStore));
+
+/* ---------------------------- get stores routes --------------------------- */
+router.get('/stores', catchErrors(storeController.getStores));
 
 module.exports = router;
